756072af117b210cd4abe0dd2bea7b7096622de4
Nadir <mutikn@mail.ru>
2025-03-11 18:00:41 +0100

    allowed hosts

--- CRM/settings.py
+++ CRM/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5pcsf2j%2*0$cxaw5a$aurc++bx+$by5%u9*fvi%ql3(g)#a#@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
