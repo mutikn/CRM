0e01378a5828bcf72864144b2d67df8ebba2903e
Nadir <mutikn@mail.ru>
2025-03-11 17:55:25 +0100

    Static files dirs

--- CRM/settings.py
+++ CRM/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5pcsf2j%2*0$cxaw5a$aurc++bx+$by5%u9*fvi%ql3(g)#a#@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
